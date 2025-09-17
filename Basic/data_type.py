d1cdbaf9fecc9cbc465d8c9007c7f4fc1ce18fe4
toji-dev <toainguyenprogramminggame@gmail.com>
2025-09-18 00:53:55 +0700

    data type

---
+++ Basic/data_type.py
@@ -0,0 +1,20 @@
+x = 3
+y = 3.14
+
+name = "Alice"
+is_student = True
+
+multiline_string = """This is a
+multiline string."""
+
+print(x)
+print(y)
+print(name)
+print(is_student)
+print(multiline_string)
+
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(is_student))
+print(type(multiline_string))
